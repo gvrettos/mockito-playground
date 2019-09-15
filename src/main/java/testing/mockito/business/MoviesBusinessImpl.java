b9ced37792d38a943cc1792d507886af9e51e987
George Vrettos <lukedevgr@gmail.com>
2019-09-15 22:10:48 +0300

    Set up the System Under Test for a JUnit 4 Mockito testing project

---
+++ src/main/java/testing/mockito/business/MoviesBusinessImpl.java
@@ -0,0 +1,27 @@
+package testing.mockito.business;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import testing.mockito.service.MoviesService;
+
+public class MoviesBusinessImpl {
+
+	private MoviesService moviesService;
+
+	public MoviesBusinessImpl(MoviesService moviesService) {
+		this.moviesService = moviesService;
+	}
+
+	public List<String> getMoviesRelatedToDie() {
+		List<String> filteredMovies = new ArrayList<>();
+		List<String> allMovies = moviesService.getMovies();
+		for (String movie: allMovies) {
+			if (movie.contains("die")) {
+				filteredMovies.add(movie);
+			}
+		}
+		return filteredMovies;
+	}
+
+}
---
+++ src/main/java/testing/mockito/service/MoviesService.java
@@ -0,0 +1,7 @@
+package testing.mockito.service;
+
+import java.util.List;
+
+public interface MoviesService {
+	List<String> getMovies();
+}
